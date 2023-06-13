b78c302639dd7178f3d04950f4c5a318d958fd3b
Aleksei <19.lebedev.aleksei.88@gmail.com>
2023-06-13 11:37:38 +0600

    Release 0-1: Client started

---
+++ client/src/main.tsx
@@ -0,0 +1,16 @@
+
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  
+  <Provider store={undefined} >
+    <BrowserRouter>  
+      <App />
+    </BrowserRouter>
+  </Provider>
+  
+)
