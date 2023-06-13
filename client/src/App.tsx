d3ccba170996d1307b079edbcecc1a91972141fa
Aleksandr Pilyugin <pilugin.aleksandr@gmail.com>
2023-06-13 17:28:01 +0300

    user profile and sittings done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import LoginSeller from './components/seller/LoginSeller/LoginSeller'
 import RegistrationSeller from './components/seller/RegistrationSeller/RegistrationSeller'
 import LoginUser from './components/user/LoginUser/LoginUser'
+import Profile from './components/user/Profile/Profile'
 import RegistrationUser from './components/user/RegistrationUser/RegistrationUser'
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
       {/* <RegistrationSeller></RegistrationSeller> */}
       {/* <LoginSeller></LoginSeller> */}
       {/* <RegistrationUser></RegistrationUser> */}
-      <LoginUser></LoginUser>
+      {/* <LoginUser></LoginUser> */}
+      <Profile></Profile>
     </>
   )
 }
