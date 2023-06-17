b42a9a982f9a32045e91feeab76d4141c8c603fb
Aleksei <19.lebedev.aleksei.88@gmail.com>
2023-06-17 17:00:04 +0600

    Release: fix btn add and btn fav on pages main and search

--- client/src/components/main/CardsPopular.tsx
+++ client/src/components/main/CardsPopular.tsx
@@ -35,7 +35,8 @@ function checkCart(id: number) {
       {card && card.map(({ id, name, price, img_url }) => (
         <article key={id} id={id} className="grid grid-cols-1 article content-between rounded-xl bg-white p-3 shadow-lg hover:shadow-xl hover:transform hover:scale-105 duration-300 ">
                  {userIsActive ? 
-                 checkFav(id) ?
+                  <>
+                 {checkFav(id) ?
                  <svg onClick={() => dispatch(removeFromFav(id))} className="addFav h-5 w-5 self-end cursor-pointer duration-150 text-red-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" >
                  <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                </svg>
@@ -43,8 +44,9 @@ function checkCart(id: number) {
                 <svg onClick={() => dispatch(addToFav(id))} className="addFav h-5 w-5 self-end cursor-pointer duration-150 hover:text-red-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" >
                   <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                 </svg>
-                 :
-                 <></>
+                 }
+                </>:
+                null
                  }
           <div className="mb-4">
             <div className="relative overflow-hidden rounded-xl">
@@ -63,7 +65,8 @@ function checkCart(id: number) {
               ₸
             </p>
             {userIsActive ?
-            checkCart(id) ?
+            <>
+            {checkCart(id) ?
   <div className="addedCart flex text-sm items-center cursor-pointer space-x-1.5 rounded-lg bg-teal-600 px-4 py-1.5 text-white duration-100">
   В корзине
 </div>
@@ -74,8 +77,12 @@ function checkCart(id: number) {
  </svg>
   В корзину
 </div>
-: <></>
+
   }
+  </>
+  :
+  null
+            }
 
           </div>
         </article>
