d0035220ad70f6f4d5ffe412c360a245cc3e699f
Aleksei <19.lebedev.aleksei.88@gmail.com>
2023-06-19 12:48:23 +0600

    Release: bug fixed

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-
 require('dotenv').config();
 
 const express = require('express');
@@ -33,9 +32,9 @@ const mapsApi = require('./src/routes/maps.api');
 // const isAuth = require('./src/middlewares/isAuth');
 
 const sessionConfig = {
-  name: "MarketPlace",
+  name: 'MarketPlace',
   store: new FileStore(),
-  secret: process.env.COOKIE_SECRET || "sometext321",
+  secret: process.env.COOKIE_SECRET || 'sometext321',
   resave: false,
   saveUninitialized: false,
   cookie: {
@@ -48,14 +47,14 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ['http://localhost:5173'],
     credentials: true,
-  })
+  }),
 );
 
 app.use(expressSession(sessionConfig));
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // app.use(express.static(path.join(__dirname, 'public/')));
