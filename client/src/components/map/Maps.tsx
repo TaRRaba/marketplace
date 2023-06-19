a355deda18bbfb247185a6cdba95baff08786aba
Natalia_Petrovna <89224822688@bk.ru>
2023-06-19 18:28:43 +0500

    refactoring maps

--- client/src/components/map/Maps.tsx
+++ client/src/components/map/Maps.tsx
@@ -1,54 +1,24 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
+import { useAppDispatch, useAppSelector } from '../../redux/store/hooks';
+import { getMaps } from '../../redux/thunks/mapsThunks/getMapsThunks';
+import { RootState } from '../../redux/store/store';
 
 export function Maps() {
 
-  // const[address, setAddress] = useState([])
+  const dispatch = useAppDispatch();
+  const address = useAppSelector((state: RootState) => state.maps.maps);
 
   useEffect(() => {
-  //   (async function() {
-  //     try {
-  //       const response = await fetch('http://localhost:3001/api/maps', {
-  //           credentials: 'include',
-  //       })
-  //       const result = await response.json();
-  //       setAddress(result)
-          
-        ymaps.ready(init)
-  //     } catch (error) {
-  //         console.log(error);
-  //     }
-  //   })()
+    dispatch(getMaps())
   },[])
 
-  // console.log("address====>", address);
-
-  //=====================================================================
-const coords = [
-  [55.742245, 37.585035], //Смоленский бульвар, 22/14, Москва
-  [55.758894, 37.609398], //Газетный переулок, 13, Москва
-  [55.721924, 37.624157], //Большая Серпуховская улица, 46с34, Москва
-  [55.727425, 37.737695], //Рязанский проспект, 2/1к5Т, Москва
-  [55.701318, 37.567159], //Ленинский проспект, 44, Москва
-  [55.759086, 37.550612], //улица Сергея Макеева, 4, Москва
-  [55.798207, 37.620106], //5-й проезд Марьиной Рощи, 15А, Москва
-  [55.782319, 37.714348], //Семёновский переулок, 18, Москва
-  [55.712455, 37.645719], //3-й Павелецкий проезд, 4, подъезд 1, Москва
-  [55.779266, 37.577766], //Ленинградский проспект, 5с7, Москва
-];
-const adress = [
-  "Смоленский бульвар, 22/14, Москва",
-  "Газетный переулок, 13, Москва",
-  "Большая Серпуховская улица, 46с34, Москва",
-  "Рязанский проспект, 2/1к5Т, Москва",
-  "Ленинский проспект, 44, Москва",
-  "улица Сергея Макеева, 4, Москва",
-  "5-й проезд Марьиной Рощи, 15А, Москва",
-  "Семёновский переулок, 18, Москва",
-  "3-й Павелецкий проезд, 4, подъезд 1, Москва",
-  "Ленинградский проспект, 5с7, Москва"
-];
-
+  useEffect(() => {
+    ymaps.ready(init) 
+  },[address])
+  
 function init() {
+  if (address.length !== 0) {
+
   const myMap = new ymaps.Map('map', {
     center: [55.752507, 37.623150],
     zoom: 11,
@@ -56,31 +26,32 @@ function init() {
     searchControlProvider: 'yandex#search',
   });
 
-const myGeoObjects = [];
+const myGeoObjects: object[] = [];
 
-for (let i = 0; i< 10; i++) {
+address.forEach((el, i) => {
   myGeoObjects[i] = new ymaps.GeoObject({
-    geometry: {
-      type: "Point",
-      coordinates: coords[i]
-    },
-    properties: {
-            // Контент метки.
-            iconContent: 'Local Market',
-            hintContent: adress[i],
-          },
-  },
-  {
-        // Иконка метки будет растягиваться под размер ее содержимого.
-        preset: 'islands#blackStretchyIcon',
-        // Метку можно перемещать.
-        draggable: false,
-      });
-}
+        geometry: {
+          type: "Point",
+          coordinates: el.coords
+        },
+        properties: {
+                // Контент метки.
+                iconContent: 'Local Market',
+                hintContent: el.address,
+              },
+      },
+      {
+            // Иконка метки будет растягиваться под размер ее содержимого.
+            preset: 'islands#blackStretchyIcon',
+            // Метку можно перемещать.
+            draggable: false,
+          });
+})
 
 const myClusterer = new ymaps.Clusterer();
 myClusterer.add(myGeoObjects);
 myMap.geoObjects.add(myClusterer);
+  }
 }
 
   return (
@@ -91,8 +62,8 @@ myMap.geoObjects.add(myClusterer);
         <div className="mx-auto grid max-w-4xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pr-8 lg:pt-4">
               <p className="mb-3 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Пункты выдачи</p>
-                {adress.map((el) => (
-                  <p key={el.length} className=' my-2'>{el}</p>
+                {address?.map((el) => (
+                  <p key={el.id} className=' my-2'>{el.address}</p>
                  ))} 
           </div>
           <div style={{height: 450}} id="map" className="  max-w-2xl rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0" />          
